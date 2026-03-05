f0df5174b82c7e7ac674d1c185204c010f8f49d2
Samuel Åkesson <samak519@student.liu.se>
2026-03-06 00:43:28 +0100

    cook further on GUI

--- gui/src/app.rs
+++ gui/src/app.rs
@@ -1,14 +1,17 @@
+use egui::{
+    Button, RichText,
+    Theme::{Dark, Light},
+};
 use serde::{Deserialize, Serialize};
+use strum::{EnumIter, IntoEnumIterator};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(default)]
 pub struct App {
-    label: String,
-
-    #[serde(skip)]
-    value: f32,
+    tab: Tab,
 }
 
+#[derive(Serialize, Deserialize, Debug, EnumIter, PartialEq)]
 enum Tab {
     Home,
     Usb,
@@ -16,20 +19,55 @@ enum Tab {
     Rcm,
 }
 
+impl Tab {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Tab::Home => "Home",
+            Tab::Usb => "USB",
+            Tab::Network => "Network",
+            Tab::Rcm => "RCM",
+        }
+    }
+
+    fn show(&self, ui: &mut egui::Ui, theme: &egui::Theme) -> egui::Response {
+        match self {
+            Tab::Home => {
+                ui.label("Select one of the tabs on the left to get started.");
+                match theme {
+                    Dark => ui.image(egui::include_image!("../../media/banner-dark.svg")),
+                    Light => ui.image(egui::include_image!("../../media/banner-light.svg")),
+                }
+            }
+            Tab::Usb => {
+                ui.label("Install through USB");
+                if ui.button("Pick file").clicked()
+                    && let Some(path) = rfd::FileDialog::new()
+                        .add_filter("*", &ironfoil_core::GAME_BACKUP_EXTENSIONS)
+                        .pick_file()
+                {
+                    eprintln!("picked file: {:?}", path);
+                } else if ui.button("Pick folder").clicked()
+                    && let Some(path) = rfd::FileDialog::new().pick_folder()
+                {
+                    eprintln!("picked folder: {:?}", path);
+                }
+                ui.label("he")
+            }
+            Tab::Network => ui.label("Network tab content goes here."),
+            Tab::Rcm => ui.label("RCM tab content goes here."),
+        }
+    }
+}
+
 impl Default for App {
     fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
+        Self { tab: Tab::Home }
     }
 }
 
 impl App {
     pub fn new(cc: &eframe::CreationContext) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        egui_extras::install_image_loaders(&cc.egui_ctx);
 
         if let Some(storage) = cc.storage {
             let stored = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
@@ -55,30 +93,41 @@ impl eframe::App for App {
             });
         });
 
-        egui::SidePanel::left("left_panel").show(ctx, |ui| {
-            ui.label("helo");
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(env!("CARGO_PKG_NAME"));
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+        egui::SidePanel::left("left_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                for tab in Tab::iter() {
+                    if self.tab == tab {
+                        if ui.button(RichText::new(tab.as_str()).strong()).clicked() {
+                            self.tab = tab;
+                        }
+                    } else if ui.button(tab.as_str()).clicked() {
+                        self.tab = tab;
+                    }
+                }
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if !matches!(self.tab, Tab::Home) {
+                    ui.heading(self.tab.as_str());
+                    ui.heading("|");
+                }
+                ui.heading(env!("CARGO_PKG_NAME"));
+            });
             ui.separator();
+
+            self.tab.show(ui, &ctx.theme());
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::RIGHT), |ui| {
                 ui.horizontal(|ui| {
                     ui.label(env!("VERGEN_GIT_DESCRIBE"));
-                    ui.hyperlink_to("ironfoil", "https://github.com/sermuns/ironfoil");
+                    ui.hyperlink_to(
+                        env!("CARGO_PKG_NAME"),
+                        "https://github.com/sermuns/ironfoil",
+                    );
                 });
+                ui.separator();
             });
         });
     }
