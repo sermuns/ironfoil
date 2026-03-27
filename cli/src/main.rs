742c0902a49663704eb779e57a946952b730bd98
Samuel Åkesson <samak519@student.liu.se>
2026-03-27 01:58:06 +0100

    refactor(cli): deduplicate usb/network install code

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,7 +3,11 @@ use indicatif::{ProgressBar, ProgressStyle};
 use ironfoil_core::{
     perform_tinfoil_network_install, perform_usb_install, read_game_paths, send_rcm_payload,
 };
-use std::{net::Ipv4Addr, path::PathBuf, sync::mpsc};
+use std::{
+    net::Ipv4Addr,
+    path::{Path, PathBuf},
+    sync::mpsc,
+};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -28,7 +32,7 @@ enum InstallType {
     #[command(arg_required_else_help = true)]
     Usb {
         #[command(flatten)]
-        transfer_args: InstallArgs,
+        install_args: InstallArgs,
 
         /// If transferring to Sphaira homebrew menu
         #[arg(long = "sphaira")]
@@ -39,7 +43,7 @@ enum InstallType {
     #[command(arg_required_else_help = true)]
     Network {
         #[command(flatten)]
-        transfer_args: InstallArgs,
+        install_args: InstallArgs,
 
         /// The IP address of the Nintendo Switch
         target_ip: Ipv4Addr,
@@ -70,47 +74,32 @@ fn main() -> color_eyre::Result<()> {
 
     let args = Cli::parse();
 
-    // TODO: deduplicate, generalise Usb and Network transfer code here...
-    // maybe create a generic functin that accepts any transfer function?
     match args.install_type {
         InstallType::Usb {
-            transfer_args,
+            install_args:
+                InstallArgs {
+                    game_backup_path,
+                    recurse,
+                },
             for_sphaira,
-        } => {
-            let pb = create_progress_bar();
-            let (progress_len_tx, progress_len_rx) = mpsc::channel::<u64>();
-            let (progress_tx, progress_rx) = mpsc::channel::<u64>();
-
-            let game_paths =
-                read_game_paths(&transfer_args.game_backup_path, transfer_args.recurse)?;
-            let usb_install_thread = std::thread::spawn(move || {
+        } => run_install(
+            &game_backup_path,
+            recurse,
+            move |game_paths, progress_len_tx, progress_tx| {
                 perform_usb_install(&game_paths, progress_len_tx, progress_tx, for_sphaira, None)
-            });
-
-            while !usb_install_thread.is_finished() {
-                if let Ok(total_len) = progress_len_rx.try_recv() {
-                    pb.set_length(total_len);
-                }
-
-                if let Ok(progress) = progress_rx.try_recv() {
-                    pb.set_position(progress);
-                }
-            }
-            usb_install_thread
-                .join()
-                .expect("joining usb install thread")?;
-        }
+            },
+        )?,
         InstallType::Network {
-            transfer_args,
+            install_args:
+                InstallArgs {
+                    game_backup_path,
+                    recurse,
+                },
             target_ip,
-        } => {
-            let pb = create_progress_bar();
-            let (progress_len_tx, progress_len_rx) = mpsc::channel::<u64>();
-            let (progress_tx, progress_rx) = mpsc::channel::<u64>();
-
-            let game_paths =
-                read_game_paths(&transfer_args.game_backup_path, transfer_args.recurse)?;
-            let network_install_thread = std::thread::spawn(move || {
+        } => run_install(
+            &game_backup_path,
+            recurse,
+            move |game_paths, progress_len_tx, progress_tx| {
                 perform_tinfoil_network_install(
                     game_paths,
                     target_ip,
@@ -118,21 +107,47 @@ fn main() -> color_eyre::Result<()> {
                     progress_tx,
                     None,
                 )
-            });
-
-            while !network_install_thread.is_finished() {
-                if let Ok(total_len) = progress_len_rx.try_recv() {
-                    pb.set_length(total_len);
-                }
-                if let Ok(progress) = progress_rx.try_recv() {
-                    pb.set_position(progress);
-                }
-            }
-            network_install_thread
-                .join()
-                .expect("joining usb install thread")?;
-        }
+            },
+        )?,
         InstallType::Rcm { payload_path } => send_rcm_payload(&payload_path)?,
     }
+    Ok(())
+}
+
+fn run_install<F>(
+    game_backup_path: &Path,
+    recurse: bool,
+    install_closure: F,
+) -> color_eyre::Result<()>
+where
+    F: FnOnce(
+            Vec<std::path::PathBuf>,
+            mpsc::Sender<u64>,
+            mpsc::Sender<u64>,
+        ) -> color_eyre::Result<()>
+        + Send
+        + 'static,
+{
+    let pb = create_progress_bar();
+
+    let game_paths = read_game_paths(game_backup_path, recurse)?;
+
+    let (progress_len_tx, progress_len_rx) = mpsc::channel::<u64>();
+    let (progress_tx, progress_rx) = mpsc::channel::<u64>();
+
+    let thread =
+        std::thread::spawn(move || install_closure(game_paths, progress_len_tx, progress_tx));
+
+    while !thread.is_finished() {
+        if let Ok(total_len) = progress_len_rx.try_recv() {
+            pb.set_length(total_len);
+        }
+
+        if let Ok(progress) = progress_rx.try_recv() {
+            pb.set_position(progress);
+        }
+    }
+
+    thread.join().expect("joining install thread")?;
     Ok(())
 }
